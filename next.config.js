9d8c16993a20f87ebf833cb5a370e15c94d0ca54
roalla <steven.robin@roalla.com>
2025-06-22 08:24:20 -0400

    fix: Correctly format Content Security Policy header

--- next.config.js
+++ next.config.js
@@ -24,15 +24,15 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value: [
-              "default-src 'self';",
-              "script-src 'self' 'unsafe-inline' https://assets.calendly.com;",
-              "style-src 'self' 'unsafe-inline' https://assets.calendly.com;",
-              "frame-src https://calendly.com;",
-              "connect-src 'self' https://calendly.com https://assets.calendly.com;",
-              "img-src 'self' data: https://assets.calendly.com;",
-              "font-src 'self' https://assets.calendly.com;",
-              "object-src 'none';",
-            ].join(" ")
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' https://assets.calendly.com",
+              "style-src 'self' 'unsafe-inline' https://assets.calendly.com",
+              "frame-src https://calendly.com",
+              "connect-src 'self' https://calendly.com https://assets.calendly.com",
+              "img-src 'self' data: https://*.calendly.com",
+              "font-src 'self'",
+              "object-src 'none'",
+            ].join("; ")
           }
         ],
       },
